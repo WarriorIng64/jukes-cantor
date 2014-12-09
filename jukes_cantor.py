681a29db3c69dee0306d710d3883b73b41bfd900
Christopher Kyle Horton <christhehorton@gmail.com>
2014-12-09 14:36:39 -0500

    Add start of main module.

---
+++ jukes_cantor.py
@@ -0,0 +1,22 @@
+# This file is part of jukes-cantor.
+# Copyright (C) 2014 Christopher Kyle Horton <chorton@ltu.edu>
+
+# jukes-cantor is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# jukes-cantor is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with jukes-cantor. If not, see <http://www.gnu.org/licenses/>.
+
+
+# MCS 5603 Intro to Bioinformatics, Fall 2014
+# Christopher Kyle Horton (000516274), chorton@ltu.edu
+# Last modified: 12/9/2014
+
+
